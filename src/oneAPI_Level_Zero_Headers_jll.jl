9454b2e2c8b86d3a1f597a23aa36e1bf683f5f83
jlbuild <juliabuildbot@gmail.com>
2026-04-27 17:29:59 +0000

    oneAPI_Level_Zero_Headers_jll build 1.15.0+0

--- src/oneAPI_Level_Zero_Headers_jll.jl
+++ src/oneAPI_Level_Zero_Headers_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("oneAPI_Level_Zero_Headers")
-JLLWrappers.@generate_main_file("oneAPI_Level_Zero_Headers", UUID("f4bc562b-d309-54f8-9efb-476e56f0410d"))
+JLLWrappers.@generate_main_file("oneAPI_Level_Zero_Headers", Base.UUID("f4bc562b-d309-54f8-9efb-476e56f0410d"))
 end  # module oneAPI_Level_Zero_Headers_jll
